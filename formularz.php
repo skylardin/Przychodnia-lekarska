7a50f5da7836e5de21263b171da36298c60c65e5
nefari0n <32513226+nefari0n@users.noreply.github.com>
2017-12-20 12:59:26 +0100

    Realizacja - plik

---
+++ formularz.php
@@ -0,0 +1,36 @@
+<html>
+<head>
+<title>Łukasz Szkaradek, Łukasz Mamak i Paweł Wilczek </title>
+
+<META http-equiv=Content-Language content=pl>
+<META http-equiv=Content-Type content="text/html; charset=UTF-8">
+<style>
+a {
+    color: black;
+    text-decoration: none;
+}
+</style>
+</head>
+
+<body>
+<center>
+<a href="index.php">Powrót</a> | <a href="recepcja.php">Zaloguj się</a>
+<body style="background-size: 100% 200%;" background="tlo.jpg" bgproperties="fixed"> <br> <br>
+<img style="margin:-7px;width: 101%; text-align:center;" src="logo.png" alt="Logo" />
+<form action="formularz.php" method="POST"><table border="2">
+<tr><td align="center" colspan="2">Formularz</td><tr>
+<tr><td>Imię:</td><td><input type="text" name="imie_osoby"></td><tr>
+<td>Nazwisko:</td><td><input type="text" name="nazwisko_osoby"></td><tr>
+<td>Pesel:</td><td><input type="text" name="pesel"></td><tr>
+<td>Numer telefonu:</td><td><input type="text" name="tel"></td><tr>
+<td align="center" colspan="2"><input type="submit" value="Wyślij"> </form></td></table>
+</body>
+<br>
+<table border="0" cellspacing="0" cellpadding="0">
+  <tr> 
+    <td style="background: rgba(42,42,42,0.4);" align="center"><font face="verdana" size="1" color="white">
+      &nbsp&nbspCopyright &copy; <b>Łukasz Szkaradek, Łukasz Mamak i Paweł Wilczek&nbsp&nbsp</b></td>
+  </tr>
+</table>
+</center>
+</html>
